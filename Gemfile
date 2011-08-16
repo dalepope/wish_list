eafdd155c0b7c497d8a45560c73d578d6d562225
Dale Pope <dale@dalepope.name>
2011-08-15 18:02:15 -0600

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'sqlite3', '1.3.3'
+
+group :development do
+  gem 'rspec-rails', '2.6.1'
+  gem 'annotate', '2.4.0'
+  gem 'faker', '0.3.1'
+end
+
+group :test do
+  gem 'rspec-rails', '2.6.1'
+  gem 'webrat', '0.7.1'
+  gem 'spork', '0.9.0.rc8'
+  gem 'autotest', '4.4.6'
+  gem 'autotest-growl', '0.2.9'
+  gem 'factory_girl_rails', '1.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WishList::Application.config.secret_token = '86a3668f16bf719df14370251a5d3d74cfd5a946caf9c5e956778bdbcbb9e3244c2b6d11b5dad999357688378bbdbd37875ac26af2c7caa5bb250db297930f72'
