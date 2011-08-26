0ed423a5720648fe9e763a81990a46263c5ed822
Dale Pope <dale@dalepope.name>
2011-08-25 18:13:50 -0600

    Added validations and password encryption to the user model class.

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,9 +17,41 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
 
   has_many :wish_items, :dependent => :destroy
-  
-  validates :password, :presence     => true,
-                       :confirmation => true,
-                       :length       => { :within => 12..40 }
 
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name, :presence => true,
+                   :length => { :maximum => 50 }
+  validates :email, :presence => true,
+                    :format => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => { :within => 12..40 }
+
+  before_save :encrypt_password
+
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  private
+  
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,6 +14,31 @@ describe UsersController do
       get :new
       response.should have_selector("title", :content => "Add User")
     end
+    
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+
+    it "should have an email field" do
+      get :new
+      response.should have_selector("input[name='user[email]'][type='text']")
+    end
+    
+    it "should have a password field" do
+      get :new
+      response.should have_selector("input[name='user[password]'][type='password']")
+    end
+    
+    it "should have a password confirmation field" do
+      get :new
+      response.should have_selector("input[name='user[password_confirmation]'][type='password']")
+    end
+    
+    it "should have an admin checkbox" do
+      get :new
+      response.should have_selector("input[name='user[admin]'][type='checkbox']")
+    end
   end
 
 end
