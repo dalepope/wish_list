5f3638bd1a53fc82f1452a2e07cd64151d0fc261
Dale Pope <dale@dalepope.name>
2011-08-26 19:06:11 -0600

    Added user show page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,14 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
+  end
   
   def index
     @users = User.all
     @title = "Wishers"
   end
-
 end
