a23a16d4c44380c65cfcf447fa7b49f7978fe035
Dale Pope <dale@dalepope.name>
2011-09-25 12:26:31 -0600

    Added some javascript to put focus on the first form element on any page.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// move focus to first form element
+window.onload = function() {
+  Form.focusFirstElement(document.forms[0]);
+}
