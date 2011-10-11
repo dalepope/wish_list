3ce61cc448f32dac138b220c01d7b2cca39721d1
Dale Pope <dale@dalepope.name>
2011-10-10 18:41:24 -0600

    Tried to make the user switching header more attractive.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,13 @@ module ApplicationHelper
   def webmaster_contact
     mail_to "dale@dalepope.name", "Dale"
   end
+  
+  def get_owned_links(owned)
+    links = ""
+    owned.each_index do |i|
+      links += ", " if i > 0
+      links += link_to owned[i].name, sessions_switch_path(:id => owned[i].id)
+    end
+    return links
+  end
 end
